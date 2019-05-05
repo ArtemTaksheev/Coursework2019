68922393a079eb8101156308808f02c51aa6119d
ArtemTaksheev <48492825+ArtemTaksheev@users.noreply.github.com>
2019-05-05 17:16:36 +0300

    Create Struct.h

---
+++ Code/Struct.h
@@ -0,0 +1,25 @@
+#ifndef COURSEWORK_STRUCT_H
+#define COURSEWORK_STRUCT_H
+typedef struct
+{
+    char* Name;//Имя
+    char* Spec;//Специальность
+    int DATE[3];//Дата рождения(yyyy/mm/dd)
+    int year_begin;//Год начала учебы
+    int group;//Номер группы
+    float average;//Максимальное среднее значение
+    float attendance;//Минимальное среднее значение
+
+}ST;
+enum sizes{MAXLINE=80};
+
+//Для работы с динамикой
+void ClearStruct (ST* MASS);//Очистка структуры
+void ClearArray (char** s, int size);//Очисткамассива строк
+//Для работы со структурой
+char** split(char* s,int size,char sep);//Разделение прочитанной строки на слова
+ST* struct_fill(char** str);//Заполнение структуры
+void print_head();//Вывод шапки таблицы
+void print_struct(ST* MASS);//Вывод структуры на экран
+
+#endif //COURSEWORK_STRUCT_H
