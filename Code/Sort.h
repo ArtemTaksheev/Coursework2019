6024f24951d9ba89aee590343a5eee6d47d972b3
ArtemTaksheev <48492825+ArtemTaksheev@users.noreply.github.com>
2019-05-05 17:16:31 +0300

    Create Sort.h

---
+++ Code/Sort.h
@@ -0,0 +1,26 @@
+#ifndef COURSEWORK_SORT_H
+#define COURSEWORK_SORT_H
+
+#include "Work_with_nodes.h"
+#include"Struct.h"
+#include "CheckValues.h"
+
+#ifdef __unix__
+#define CLS system("clear");
+#elif WIN32
+#define  CLS system("cls");
+#endif
+void Sort(LH* Head);
+void SortKind(LH*Head,float(*funcName)(LN*,LN*));
+void sortDate (LH*Head);
+int DataLook (LN *Node1, LN *Node2);
+
+float NameValue(LN* Node1,LN*Node2);
+float SpecValue(LN* Node1,LN*Node2);
+float YearBegValue(LN* Node1,LN*Node2);
+float GroupValue(LN* Node1,LN*Node2);
+float AvgValue(LN *Node1, LN *Node2);
+float AttendanceValue(LN *Node1, LN *Node2);
+
+
+#endif //COURSEWORK_SORT_H
